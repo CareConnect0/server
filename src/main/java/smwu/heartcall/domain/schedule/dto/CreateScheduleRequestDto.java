3edf653e6b2a9a79632ebdd467c5a68f6c8231b1
tichall <0817frog@gmail.com>
2025-05-05 18:58:42 +0900

    fix #11 : Request Dto 정규식 수정
    - 날짜 형식 정규식 수정
    - parse String 형식 수정

--- src/main/java/smwu/heartcall/domain/schedule/dto/CreateScheduleRequestDto.java
+++ src/main/java/smwu/heartcall/domain/schedule/dto/CreateScheduleRequestDto.java
@@ -1,6 +1,7 @@
 package smwu.heartcall.domain.schedule.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,9 @@ public class CreateScheduleRequestDto {
     private String content;
 
     @NotBlank
+    @Pattern(
+            regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}$",
+            message = "날짜 형식은 YYYY-MM-DD HH:mm 이어야 합니다."
+    )
     private String startTime;
 }
