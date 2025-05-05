a73a93ac32d0921a9624eef59a2a46ad19cb6965
tichall <0817frog@gmail.com>
2025-05-05 23:28:22 +0900

    fix #11 : Remove unused import statement

--- src/main/java/smwu/heartcall/global/exception/errorCode/SecurityErrorCode.java
+++ src/main/java/smwu/heartcall/global/exception/errorCode/SecurityErrorCode.java
@@ -1,6 +1,5 @@
 package smwu.heartcall.global.exception.errorCode;
 
-import com.google.api.Http;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
