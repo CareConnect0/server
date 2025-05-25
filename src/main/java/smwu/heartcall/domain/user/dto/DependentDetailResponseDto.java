d40027d974658b9fd97ab341716e1708db8fcf2a
tichall <0817frog@gmail.com>
2025-05-25 14:24:14 +0900

    fix #33 : 피보호자 조회 반환값 수정

--- src/main/java/smwu/heartcall/domain/user/dto/DependentDetailResponseDto.java
+++ src/main/java/smwu/heartcall/domain/user/dto/DependentDetailResponseDto.java
@@ -1,5 +1,6 @@
 package smwu.heartcall.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import smwu.heartcall.domain.user.entity.DependentRelation;
@@ -12,12 +13,16 @@ public class DependentDetailResponseDto {
     private String username;
     private String name;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String profileUrl;
+
     public static DependentDetailResponseDto of(DependentRelation relation) {
         User dependent = relation.getDependent();
         return DependentDetailResponseDto.builder()
                 .dependentId(dependent.getId())
                 .username(dependent.getUsername())
                 .name(dependent.getName())
+                .profileUrl(dependent.getProfileUrl())
                 .build();
     }
 }
