b8b8df7b3a1940705fbe7de2a38bdc18cc08b769
tichall <0817frog@gmail.com>
2025-05-25 14:24:23 +0900

    fix #33 : 채팅 상대 조회 반환값 수정

--- src/main/java/smwu/heartcall/domain/chat/dto/TargetResponseDto.java
+++ src/main/java/smwu/heartcall/domain/chat/dto/TargetResponseDto.java
@@ -1,5 +1,6 @@
 package smwu.heartcall.domain.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import smwu.heartcall.domain.user.entity.User;
@@ -10,10 +11,14 @@ public class TargetResponseDto {
     private Long userId;
     private String name;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String profileUrl;
+
     public static TargetResponseDto of(User user) {
         return TargetResponseDto.builder()
                 .userId(user.getId())
                 .name(user.getName())
+                .profileUrl(user.getProfileUrl())
                 .build();
 
     }
